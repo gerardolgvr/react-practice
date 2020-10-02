002fa53375bef7c0ff7777962af2193684ce683c
Gerardo Vazquez <gerardolgvr0310@gmail.com>
2020-10-01 19:27:19 -0500

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            author: {
+                name: '',
+                birthdate: '',
+            },
+            categories: [],
+            flavors: [],
+            value: 'lime',
+            loading: true,
+        };
+
+        this.onChangeAuthor = this.onChangeAuthor.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.onChangeCheckbox = this.onChangeCheckbox.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ author: this.props.author});
+        this.setState({ categories: this.props.authorCategories});  // esto puede hacerse en un solo setState
+        this.setState({ flavors: this.props.flavors});
+        this.setState({ loading: false});
+
+        console.log('component did mount and props', this.props);
+    }
+
+    onChangeAuthor = (event) => {
+        this.setState({ authorName:  event.target.value})
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        // whatever request you have to do
+        alert('Your form has been sent');
+        event.preventDefault();
+    }
+
+    onChangeCheckbox = (i) => (event) =>{
+        const auxCategories = [ ...this.state.categories ];
+        auxCategories[i].isChecked = !auxCategories[i].isChecked;
+        this.setState({ categories: auxCategories });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="author">Author:</label>
+                    <input id="author" onChange={this.onChangeAuthor} value={this.state.author.name} type="text" />
+
+                    <br/>
+                    <br/>
+
+                    <label>
+                        Pick your favorite flavor:
+                        <select value={this.state.value} onChange={this.handleChange}>
+                            { !this.state.loading && (
+                                this.state.flavors.map((flavor, i) =>{
+                                    return(
+                                        <option key={flavor.id} value={flavor.label}>{flavor.field}</option>
+                                    )
+                                })
+                            )}
+                        </select>
+                    </label>
+
+                    <br/>
+                    <br/>
+
+                    { !this.state.loading  && (
+                        this.state.categories.map((category, i) =>{
+                            return(
+                                <label key={category.id} className="checkbox-checked">
+                                    <input
+                                        type="checkbox"
+                                        checked={category.isChecked}
+                                        onChange={this.onChangeCheckbox(i)}
+                                        value={category.name}/>
+                                    <span className="label-text">{category.name}</span>
+                                </label>
+                            )
+                        })
+                    )}
+
+                    <br/>
+                    <br/>
+                    <input type="submit" value="Submit" />
+
+                </form>
+
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,50 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+const author = {
+    name: 'Gerardo Vazquez',
+    birthdate: '31 - ago - 2020',
+}
+const categories = [
+    {
+        id: 1,
+        name: 'Terror',
+        isChecked: true
+    },
+    {
+        id: 2,
+        name: "Romanticas",
+        isChecked: false
+    },
+    {
+        id: 3,
+        name: 'Comedia',
+        isChecked: true
+    }
+]
+
+const flavors = [
+    {
+        id: 1,
+        label: 'grapefruit',
+        field: 'Grapefruit',
+    },
+    {
+        id: 2,
+        label: 'lime',
+        field: 'Lime',
+    },
+    {
+        id: 3,
+        label: 'coconut',
+        field: 'Coconut',
+    }
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App author={author} authorCategories={categories} flavors={flavors} />
   </React.StrictMode>,
   document.getElementById('root')
 );
